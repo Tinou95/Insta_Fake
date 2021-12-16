c27028106997297446b322665e131a39a1251385
unknown <quentinadrieu@yahoo.com>
2021-12-16 17:18:37 +0100

    page_insta_debut

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  const sign_in = () => {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
   if ( username == "Formget" && password == "formget#123"){
   alert ("Login successfully");
   window.location = "insta_page.html";
   return false;
-  }
+
+  }}
 
   });
   
