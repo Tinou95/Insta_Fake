9a17bf40ea403d882a01981c8024b6a222881579
unknown <quentinadrieu@yahoo.com>
2021-12-16 18:08:39 +0100

    Hr

--- js/app.js
+++ js/app.js
@@ -1,14 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const sign_in = () => {
-  let username = document.getElementById("username").value;
+
+  let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
-  if ( username == "Formget" && password == "formget#123"){
+  let btn = document.querySelector(".btn");
+
+  btn.addEventListener("click", () =>{
+  const sign_in = () => {
+ 
+  if ( email == "quentin@gmail.com" && password == "quentin#123"){
   alert ("Login successfully");
   window.location = "insta_page.html";
   return false;
 
-  }}
+  }
+}
 
-  });
+  })
+
+
+
+  })
   
